be2d5e13ba5c534cc530d729491436eda6552c59
Matheus Andre <matheusandr2@gmail.com>
2024-10-08 01:43:53 -0300

    adicionando exercicios 4 e 5

---
+++ Logica_De_Programacao/Semana1/atividade05.py
@@ -0,0 +1,5 @@
+temperatura = float(input('Digite a Temperatura em Celsius? '))
+
+fahrenheit  = (temperatura * 9/5) + 32;
+
+print('A temperatura em Fahrenheit é: ', fahrenheit)
---
+++ Logica_De_Programacao/Semana1/atividade4.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite seu primeiro numero: "))
+num2 = int(input("Digite seu segundo numero: "))
+num3 = int(input("Digite seu terceiro numero: "))
+
+media = num1 + num2 + num3 / 3
+print("A média é ", media)
