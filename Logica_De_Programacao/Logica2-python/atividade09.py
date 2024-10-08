2c8d1b918dae57726fe9c4230dc77a907f560a28
Matheus Andre <matheusandr2@gmail.com>
2024-10-08 16:17:00 -0300

    correcao

--- Logica_De_Programacao/Logica2-python/atividade09.py
+++ Logica_De_Programacao/Logica2-python/atividade09.py
@@ -1,3 +1,6 @@
-frase = input()
+frase = str(input())
+caractere = str(input())
 
+contagem = frase.count(caractere);
 print("A Frase tem ", len(frase), "caracteres")
+print("A Caracter Aparece ", contagem, "caracteres iguais a ", contagem)
